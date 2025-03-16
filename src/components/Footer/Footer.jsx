8299f90feb8d25b141f2b333a4b64e5a48ca5c89
Farhan8928 <149481746+Farhan8928@users.noreply.github.com>
2025-03-16 06:25:29 -0700

    Updated footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,23 @@
-  import React from "react";
-  import "./Footer.css";
-  import Wave from "../../img/wave.png";
+import React from "react";
+import "./Footer.css";
+import Wave from "../../img/wave.png";
 
-  const Footer = () => {
-    return (
-      <div className="footer">
-        <img src={Wave} alt="" style={{ width: "100%" }} />
-        <div className="f-content">
-          <span>experts.research01@gmail.com</span>
-          <div
-            className="f-address"
-            style={{ display: "flex", flexDirection: "column" }}
-          >
-            <span>Office Address : </span>
-            <span>
-              4th Floor, Banarsi Heritage Office, 405-406, Malad, Mindspace, Malad
-              West, Mumbai, Maharashtra 400064
-            </span>
-          </div>
-          {/* <div className="f-icons">
-            <Insta color="white" size={"3rem"} />
-            <Facebook color="white" size={"3rem"} />
-            <Gitub color="white" size={"3rem"} />
-          </div> */}
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>experts.research01@gmail.com</span>
+        <div className="f-address">
+          <span>Office Address: </span>
+          <span>
+            4th Floor, Banarsi Heritage Office, 305-306,
+            Mindspace, Malad West, Mumbai, Maharashtra 400064
+          </span>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Footer;
+export default Footer;
